2a5a9881fdd9655be4c62a523ae964e7d0b4fa82
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-02 17:39:18 -0300

    feat: finish hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,84 @@
+import { useBreakpoint } from "gatsby-plugin-breakpoints";
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+
+export default function Hero() {
+  const breakpoints = useBreakpoint();
+
+  return (
+    <section
+      style={{
+        backgroundImage: 'url("../../images/hero-background.png")',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "bottom right",
+        display: "flex",
+        flexDirection: breakpoints.sm ? "column" : "row",
+        alignItems: breakpoints.sm ? "center" : "end",
+        padding: breakpoints.md ? "2rem 1rem 0" : "3rem 8rem 0",
+      }}
+    >
+      <div
+        style={{
+          textAlign: breakpoints.sm ? "center" : "start",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: breakpoints.sm ? "center" : "start",
+          gap: "2rem",
+          padding: "2rem 0",
+          marginBottom: "auto",
+        }}
+      >
+        <h2
+          style={{
+            fontSize: breakpoints.sm ? "2.5rem" : "3.5rem",
+            maxWidth: "min(80%, 580px)",
+          }}
+        >
+          A solução perfeita{" "}
+          <span style={{ textTransform: "uppercase", fontWeight: "900" }}>
+            para <span style={{ color: "var(--clr-secondary)" }}>você!</span>
+          </span>
+        </h2>
+        <p
+          style={{
+            textAlign: breakpoints.sm ? "center" : "start",
+            fontSize: "1.125rem",
+            maxWidth: "400px",
+          }}
+        >
+          Oferecemos as soluções ideais em TI, segurança, limpeza e mais!
+        </p>
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <button
+            style={{
+              fontSize: "1.25rem",
+              color: "var(--clr-white)",
+              background: "var(--brand-gradient)",
+              padding: ".25rem 1rem",
+              borderRadius: "25px",
+            }}
+          >
+            Nossos serviços
+          </button>
+          <button
+            style={{
+              fontSize: "1.25rem",
+              backgroundColor: "transparent",
+              border: "2px solid var(--clr-primary)",
+              padding: ".15rem 1rem",
+              borderRadius: "25px",
+            }}
+          >
+            Contato
+          </button>
+        </div>
+      </div>
+      <StaticImage
+        alt="Homem sorrindo"
+        src="../../images/hero-photo.png"
+        style={{ maxWidth: "650px" }}
+      />
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import "../styles/main.css";
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 };
